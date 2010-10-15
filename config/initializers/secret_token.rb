f92b40c03bd01077fc38a76e5002ac9c41dbad61
GForce <Gerald.Zhou@gmail.com>
2010-10-15 15:27:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstapp::Application.config.secret_token = 'c1d1365e3d92361d3e89bd0c4ed6f9180a6be67faff2d0778a34855caff36b46148ef029d180a56e9bcd1d60196573604f46dc8e69a59612ae2cd550dc2bd43e'
